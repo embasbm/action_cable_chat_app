2e79d9e9ebf9508d5b4503050bfb1c53eca3f767
Emba Moussa <embasbm@gmail.com>
2017-03-20 13:06:00 +0100

    new model Dialect added

---
+++ app/models/dialect.rb
@@ -0,0 +1,3 @@
+class Dialect < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719235623) do
+ActiveRecord::Schema.define(version: 20170320115422) do
+
+  create_table "dialects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
